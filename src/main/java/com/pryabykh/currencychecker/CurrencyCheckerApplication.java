765fc6cf6a79f44c4f80c1e182dcee9bcb61dc58
Pavel Ryabykh <pryabykh@gmail.com>
2022-06-02 18:24:15 +0300

    initialize Spring

---
+++ src/main/java/com/pryabykh/currencychecker/CurrencyCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.pryabykh.currencychecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pryabykh/currencychecker/CurrencyCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pryabykh.currencychecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
